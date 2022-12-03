2cabfc50ac7f6d4dbcb06d1daf5a0c43ee493a44
TakakiMoriguchi <entersight@gmail.com>
2022-12-03 04:47:09 +0000

    now open

--- pages/index.vue
+++ pages/index.vue
@@ -9,14 +9,10 @@
           <h2>データそのまま！<br />最短30分で即日修理。</h2>
           <h5>予約制（in work and place）</h5>
 
-           <!--
           <v-btn color="deep-purple lighten-2 mt-2" depressed>
             <a href="tel:07043192480">今すぐ電話する！</a>
           </v-btn>
-          -->
 
-          <h1 class="my-2 red">休業中</h1>
-          <span>11月上旬再開予定</span>
         </div>
       </v-container>
     </v-container>
@@ -27,7 +23,6 @@
       <index-price />
       <index-map />
     </section>
-
 
     <global-footer />
   </main>
